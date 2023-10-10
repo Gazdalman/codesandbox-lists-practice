693a2d496eba8c34fc8fbc4d690c57701d11ae7e
Toney Winston <137526632+Gazdalman@users.noreply.github.com>
2023-10-10 13:14:39 -0500

    Initial commit

---
+++ src/PokeMoves.js
@@ -0,0 +1,16 @@
+import PokeMoveCard from "./PokeMoveCard";
+import { moves } from "./data";
+const PokeMoves = () => {
+  return (
+    <div>
+      <h1>PokeMoves</h1>
+      <ul>
+        {moves.map((item) => (
+          <PokeMoveCard key={item.id} {...item} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PokeMoves;
